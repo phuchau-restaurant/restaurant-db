424977913804df96442958ed63320b2bcbe3f435
HarryNguyen <153167571+BDCALM@users.noreply.github.com>
2025-10-13 13:30:54 +0700

    Add files via upload

---
+++ BTVN_Hau/tailwind.config.js
@@ -0,0 +1,13 @@
+//declare a text-border
+module.exports = {
+  theme: {
+    extend: {
+      textBorder: {
+        DEFAULT: '1px solid rgba(0, 0, 0, 0.1)',
+      },
+      boxShadow: {
+        DEFAULT: '0 10px 25px rgba(0, 0, 0, 0.3)',
+      },
+    },
+  },
+}
